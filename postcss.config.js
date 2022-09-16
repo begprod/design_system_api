b5c68345713b33712421246238d3cab096608c9c
Andrey Aratov <beg431@gmail.com>
2022-09-16 20:04:17 +0300

    added packages, colors demo

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,10 @@ module.exports = {
 	plugins: [
 		require("postcss-import"),
 		require("postcss-nested"),
+		require("postcss-simple-vars"),
+		require("postcss-hexrgba"),
 		require("postcss-custom-media"),
+		require("postcss-reporter"),
 		...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
 	],
 };
